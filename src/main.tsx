e37f7cb458ba74fdc0ec0aafb38811eba2c35e66
Júlia Lima <juliafclima@hotmail.com>
2025-10-31 19:49:36 -0300

    fix: retirando lib incompativel

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
 import "./index.css";
@@ -10,13 +9,11 @@ import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
-      <HelmetProvider>
-         <BrowserRouter>
-            <ThemeProvider theme={theme}>
-               <CssBaseline />
-               <App />
-            </ThemeProvider>
-         </BrowserRouter>
-      </HelmetProvider>
+      <BrowserRouter>
+         <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+         </ThemeProvider>
+      </BrowserRouter>
    </React.StrictMode>,
 );
