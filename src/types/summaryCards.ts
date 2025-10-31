6973802e4192ae88a466e6363098d621a1bb368f
Júlia Lima <juliafclima@hotmail.com>
2025-10-31 19:54:26 -0300

    fix: erro na tipagem

--- src/types/summaryCards.ts
+++ src/types/summaryCards.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export type SummaryItem = {
    label: string;
    value: number;
-   icon: React.ComponentType<unknown>;
+   icon: React.ComponentType<any>;
    color: string;
    bgColor: string;
 };
